961c828628f9d83efc7f78922875df59ede827af
swati <scim1user@gmail.com>
2023-02-14 15:14:01 +0530

    testng code

---
+++ src/test/java/testng/balance.java
@@ -0,0 +1,16 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class balance {
+
+    @Test
+    public void shopLoan(){
+        System.out.println("shop loan");
+    }
+
+    @Test
+    public void homeLoan(){
+        System.out.println("home loan");
+    }
+}
---
+++ src/test/java/testng/basic.java
@@ -0,0 +1,16 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class basic {
+
+    @Test
+    public void carLoan(){
+        System.out.println("car loan");
+    }
+
+    @Test
+    public void bikeLoan(){
+        System.out.println("bike loan");
+    }
+}
