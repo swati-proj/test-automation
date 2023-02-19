e4dafc8fe3eb7462bf89ccb43b4e0dc6184267c6
swati <scim1user@gmail.com>
2023-02-19 19:41:46 +0530

    added groups

--- src/test/java/testng/balance.java
+++ src/test/java/testng/balance.java
@@ -1,9 +1,14 @@
 package testng;
 
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class balance {
 
+    @BeforeSuite
+    public void suite(){
+        System.out.println("Before Suite");
+    }
     @Test
     public void shopLoan(){
         System.out.println("shop loan");
--- src/test/java/testng/basic.java
+++ src/test/java/testng/basic.java
@@ -1,16 +1,45 @@
 package testng;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class basic {
 
-    @Test
-    public void carLoan(){
-        System.out.println("car loan");
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Java programs");
     }
 
+    @Test(dependsOnMethods = {"reverseString"})
+    public void charCount(){
+       String s = "javatpoint java programming";
+       int c =0;
+       for (int i=0;i<s.length();i++){
+           if(s.charAt(i) != ' ')
+               c++;
+           }
+        System.out.println(c);
+       }
+
+       @BeforeTest
+     public void prerequisite(){
+           System.out.println("Before programs");
+       }
+
     @Test
-    public void bikeLoan(){
-        System.out.println("bike loan");
+    public void reverseString(){
+        String str = "selenium";
+        String rev=" ";
+        for (int i = str.length()-1; i >=0 ; i--) {
+            rev = rev+str.charAt(i);
+        }
+        System.out.println(rev);
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("After programs");
     }
 }
